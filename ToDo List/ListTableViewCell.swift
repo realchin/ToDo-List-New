f0f0f59b420f98e95e801d722227b25016018a85
Timothy Chin <chinti@bc.edu>
2022-02-25 16:25:15 -0500

    Section 5.12

--- ToDo List/ListTableViewCell.swift
+++ ToDo List/ListTableViewCell.swift
@@ -13,10 +13,19 @@ protocol ListTableViewCellDelegate: class {
 
 class ListTableViewCell: UITableViewCell {
     
-    weak var delegate: ListTableViewCellDelegate?
+
 
     @IBOutlet weak var checkBoxButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
+    
+    weak var delegate: ListTableViewCellDelegate?
+    
+    var toDoItem: ToDoItem! {
+        didSet {
+            nameLabel.text = toDoItem.name
+            checkBoxButton.isSelected = toDoItem.completed
+        }
+    }
     
     @IBAction func checkToggled(_ sender: UIButton) {
         
--- ToDo List/ToDoListViewController.swift
+++ ToDo List/ToDoListViewController.swift
@@ -208,8 +208,7 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource, Li
         print("🚣‍♀️ cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoItems[indexPath.row])")
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ListTableViewCell
         cell.delegate = self
-        cell.nameLabel.text = toDoItems[indexPath.row].name
-        cell.checkBoxButton.isSelected = toDoItems[indexPath.row].completed
+        cell.toDoItem = toDoItems[indexPath.row]
         return cell
         
     }
