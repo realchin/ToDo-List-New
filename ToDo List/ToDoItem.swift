13ada2e8b15fdfb54f89333ecaa016e89832b191
Timothy Chin <chinti@bc.edu>
2022-02-25 14:15:18 -0500

    Section 5.10, 5.11

--- ToDo List/ToDoItem.swift
+++ ToDo List/ToDoItem.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 struct ToDoItem: Codable {
+    
     var name: String
     var date: Date
     var notes: String
     var reminderSet: Bool
     var notificationID: String?
     var completed: Bool
+
 }
