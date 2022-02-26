b643f356fadaea45311cb53a63e84c17bc4e917d
Timothy Chin <chinti@bc.edu>
2022-02-25 22:06:24 -0500

    Section 5.17

--- ToDo List/ToDoDetailTableViewController.swift
+++ ToDo List/ToDoDetailTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 private let dateFormatter: DateFormatter = {
     
@@ -36,6 +37,10 @@ class ToDoDetailTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // setup foreground notification
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(appActiveNotification), name: UIApplication.didBecomeActiveNotification, object: nil)
+        
         // hide keyboard if we tap outside of a field
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
         tap.cancelsTouchesInView = false
@@ -51,6 +56,13 @@ class ToDoDetailTableViewController: UITableViewController {
         }
         
         updateUserInterface()
+        
+    }
+    
+    @objc func appActiveNotification() {
+        
+        print("😮 The app just came to the foreground - cool!")
+        updateReminderSwitch()
         
     }
     
